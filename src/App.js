3b3647fe8751a13674822fb2a2264b309cd19a56
htactive-dieuqt <dieuqt@htactive.com>
2020-08-04 14:23:26 +0700

    + - ok

--- src/App.js
+++ src/App.js
@@ -1,12 +1,49 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { incrementAction, decrementAction} from './actions';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      App
-    </div>
-  );
+class App extends Component {
+	onDecrement() {
+		this.props.onDecrement(1);
+	}
+
+	onIncrement() {
+		this.props.onIncrement(1)
+	}
+
+	render() {
+		return (
+			<div className="App">
+				<header className="App-header">
+					<div className="container">
+						<div style={{marginBottom: '15px'}}>
+							<button onClick={this.onDecrement.bind(this)} style={{marginRight: '15px'}} type="button" className="btn btn-info">Giảm</button>
+							<button onClick={this.onIncrement.bind(this)} type="button" className="btn btn-info">Tăng</button>
+						</div>
+						<div>Counts: <span>{this.props.times}</span></div>
+					</div>
+				</header>
+			</div>
+		);
+	}
 }
 
-export default App;
+const mapStateToProps = (state) => {
+	return {
+		times: state.counterReducers ? state.counterReducers : 0
+	};
+}
+
+const mapDispatchToProps = (dispatch) => {
+	return {
+		onDecrement: (step) => {
+			dispatch(decrementAction(step));
+		},
+		onIncrement: (step) => {
+			dispatch(incrementAction(step));
+		}
+	};
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
--- src/index.js
+++ src/index.js
@@ -3,12 +3,29 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import rootSaga from './sagas/rootSaga';
+//Redux
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import myReducer from './reducers';
+//Redux Saga
+import createSagaMiddleware from 'redux-saga'; // hàm này có nhiệm vụ tạo ra một middleware năm giữa action và reducer trong redux
+// Middleware
+const sagaMiddleware = createSagaMiddleware(); //
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+var store = createStore(
+    myReducer,
+    applyMiddleware(sagaMiddleware)
 );
 
+ReactDOM.render(
+    <Provider store={ store }>
+        <App />
+    </Provider>,
+    document.getElementById('root')
+);
+
+sagaMiddleware.run(rootSaga); // Hàm này là chạy các saga
+
 serviceWorker.unregister();
+
